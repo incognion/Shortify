d73734912c4279f7775b4c5153c4fd66dd3f39f1
Ashutosh Kumar <forjeeprep@gmail.com>
2025-07-12 16:29:02 +0530

    cors policy

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: 'https://shortifyplus.onrender.com',
-    methods: ['GET', 'POST', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
